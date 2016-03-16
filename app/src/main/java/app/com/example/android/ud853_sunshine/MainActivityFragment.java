c49e53d475e489a861fad004e2a171691eb63629
Animesh Kumar Bhadra <animesh.bhadra@gmail.com>
2016-03-17 00:01:17 +0530

    Completed the first lesson on Android.

--- app/src/main/java/app/com/example/android/ud853_sunshine/MainActivityFragment.java
+++ app/src/main/java/app/com/example/android/ud853_sunshine/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -48,6 +49,9 @@ public class MainActivityFragment extends Fragment {
 
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
 //        return inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
     }
